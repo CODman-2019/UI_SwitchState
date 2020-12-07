1f8f4f0cc79785768bc9cf827699af0e9ce13db3
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2020-12-07 11:14:59 -0400

    FInishing up + created build

---
+++ UI_SwitchStateMenu/Assets/Scripts/TweenAnimations/Bonucing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonucing : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void OnMouseOver()
+    {
+        LeanTween.scale(this.gameObject, new Vector3(0.5f, 0.5f, gameObject.transform.localScale.z), 500f).setEase(LeanTweenType.easeInBounce);
+    }
+}
